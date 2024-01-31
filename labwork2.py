8ad571375c7228889aee60676192291316478fae
Aizere29906 <158247183+Aizere29906@users.noreply.github.com>
2024-01-31 18:30:20 +0600

    Add files via upload

---
+++ labwork2.py
@@ -0,0 +1,91 @@
+#exercise1
+print("Hello World")
+
+#exercise2
+if 5 > 2:
+    print("YES")
+    
+#exercise3
+#This is a comment
+
+#exercise4
+""""
+This is a comment
+written in 
+more than just one line
+"""
+
+#exercise5
+carname = "Volvo"
+
+#exercise6
+x = 50 
+
+#exercise7
+x = 5
+y = 10
+print(x+y)
+
+#exercise8
+x = 5
+y = 10
+z = x+y
+print(z)
+
+#exercise9
+x, y, z = "Orange", "Banana", "Cherry"
+
+#exercise10
+x = y = z = "Orange"
+
+#exercise11
+def myfunc():
+    global x
+    x = "fantastic"
+
+#exercise12
+x = 5
+print(type(x))
+int
+
+#exercise13
+x = "Hello World"
+print(type(x))
+str
+
+#exercise14
+x = 20.5
+print(type(x))
+float
+
+#exercise15
+x = ["apple", "banana", "cherry"]
+print(type(x))
+list
+
+#exercise16
+x = ("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+#exercise17
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+#exercise18
+x = True
+print(type(x))
+bool
+
+#exercise19
+x = 5
+x = float(x)
+
+#exercise20
+x = 5.5
+x = int(x)
+
+#exercise21
+x = 5
+x = complex(x)
